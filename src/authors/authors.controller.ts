58ca6549d4cb7a3704028f90f870ad9979c5ff5e
dothienlinh <dothienlinh2004thd@gmail.com>
2024-06-23 01:29:41 +0700

    create decorater format response before sending

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -12,6 +12,7 @@ import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ResponseMessage } from 'src/decorators/responseMessage.decorator';
 
 @ApiTags('Author')
 @Controller('authors')
@@ -38,24 +39,28 @@ export class AuthorsController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new author' })
+  @ResponseMessage('Create a new author successfully!')
   create(@Body() createAuthorDto: CreateAuthorDto) {
     return this.authorsService.create(createAuthorDto);
   }
 
   @Put(':id')
   @ApiOperation({ summary: 'Update author by id' })
+  @ResponseMessage('Update an authors successfully!')
   update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
     return this.authorsService.update(+id, updateAuthorDto);
   }
 
   @Patch('/restore/:id')
   @ApiOperation({ summary: 'Restore author by id' })
+  @ResponseMessage('Restore an authors successfully!')
   restore(@Param('id') id: number) {
     return this.authorsService.restore(+id);
   }
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete author by id' })
+  @ResponseMessage('Delete an authors successfully!')
   remove(@Param('id') id: string) {
     return this.authorsService.remove(+id);
   }
